c5fb9018f1804b50ea9a989163cd18240bedf4ba
wstarp <wangxingpeng@126.com>
2018-04-16 08:21:37 +0800

    解决报错

--- src/main/java/com/tech/lfy/service/EnglishWordService.java
+++ src/main/java/com/tech/lfy/service/EnglishWordService.java
@@ -4,10 +4,11 @@ import com.tech.lfy.mapper.EnglishWordMapper;
 import com.tech.lfy.model.EnglishWordEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
-
+@Service
 public class EnglishWordService {
     protected final Logger logger = LoggerFactory.getLogger(this.getClass());
     @Resource
