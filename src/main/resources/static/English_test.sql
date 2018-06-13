d88fd8391e35ea8dcc4212977ea6205398b25c68
zhangmingwei <1374095073@qq.com>
2018-06-13 16:57:25 +0800

    增加答题

--- src/main/resources/static/English_test.sql
+++ src/main/resources/static/English_test.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50718
 File Encoding         : 65001
 
-Date: 2018-06-12 19:12:55
+Date: 2018-06-13 14:57:43
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,13 +23,13 @@ CREATE TABLE `English_test` (
   `id` varchar(50) CHARACTER SET latin1 NOT NULL COMMENT 'ID',
   `questionno` int(11) DEFAULT NULL COMMENT '题号',
   `question` varchar(1000) CHARACTER SET utf8 COLLATE utf8_unicode_520_ci DEFAULT NULL COMMENT '问题',
-  `testtype` varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_520_ci DEFAULT '' COMMENT '问题类型（1选择，2简答）',
-  `answera` varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_520_ci DEFAULT '' COMMENT '答案A',
-  `answerb` varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_520_ci DEFAULT '' COMMENT '答案B',
-  `answerc` varchar(1000) CHARACTER SET utf8 COLLATE utf8_unicode_520_ci DEFAULT '' COMMENT '答案C',
-  `answerd` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_520_ci DEFAULT '' COMMENT '答案D',
-  `answertrue` varchar(255) DEFAULT NULL COMMENT '正确答案',
-  `testkey` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_520_ci DEFAULT NULL COMMENT '第几号试卷',
+  `testtype` varchar(2) CHARACTER SET utf8 COLLATE utf8_unicode_520_ci DEFAULT '' COMMENT '问题类型（1选择，2简答）',
+  `answera` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_520_ci DEFAULT '' COMMENT '答案A',
+  `answerb` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_520_ci DEFAULT '' COMMENT '答案B',
+  `answerc` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_520_ci DEFAULT '' COMMENT '答案C',
+  `answerd` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_520_ci DEFAULT '' COMMENT '答案D',
+  `answertrue` varchar(100) DEFAULT NULL COMMENT '正确答案',
+  `testkey` varchar(2) CHARACTER SET utf8 COLLATE utf8_unicode_520_ci DEFAULT NULL COMMENT '试卷级别（1小学，2中学，3初中，4高中，5大学）',
   `testid` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_520_ci DEFAULT NULL COMMENT '试卷编号',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -37,8 +37,8 @@ CREATE TABLE `English_test` (
 -- ----------------------------
 -- Records of English_test
 -- ----------------------------
-INSERT INTO `English_test` VALUES ('1', '1', 'are you OK?', '1', 'A YES', 'B NO', 'C I\'m not sure', 'D HAHA', 'C', '1', null);
-INSERT INTO `English_test` VALUES ('10', '10', 'are you OK?', '1', 'A YES', 'B NO', 'C I\'m not sure', 'D HAHA', 'C', '1', null);
+INSERT INTO `English_test` VALUES ('1', '1', 'are you OK?', '1', 'A YES', 'B NO', 'C I am not sure', 'D HAHA', 'C', '1', null);
+INSERT INTO `English_test` VALUES ('10', '10', 'are you OK?', '1', 'A YES', 'B NO', 'C I am not sure', 'D HAHA', 'C', '1', null);
 INSERT INTO `English_test` VALUES ('11', '11', 'are you OK?', '1', 'A YES', 'B NO', 'C I\'m not sure', 'D HAHA', 'C', '1', null);
 INSERT INTO `English_test` VALUES ('12', '12', 'are you OK?', '1', 'A YES', 'B NO', 'C I\'m not sure', 'D HAHA', 'C', '1', null);
 INSERT INTO `English_test` VALUES ('13', '13', 'are you OK?', '1', 'A YES', 'B NO', 'C I\'m not sure', 'D HAHA', 'C', '1', null);
